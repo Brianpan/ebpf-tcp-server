f7ca7fcdd8ed17738789218de5f430e219f23339
YSRossi <cool521525@gmail.com>
2023-06-24 11:24:45 +0800

    Add a redirection method
    The original method redirects the packets to the egress interface of
    the server socket when the client and server are on the same computer
    or on different computers. In fact, if the client and server are on the
    same computer, the server socket can redirect the packet to the client
    socket directly when it receives the packet.
    
    Therefore, the new method will make different redirection operations
    for the client and server are on the same computer and different
    computers.

--- bpf_redir.c
+++ bpf_redir.c
@@ -13,22 +13,31 @@ int bpf_redir(struct __sk_buff * skb)
     
     if (skb->len == 0)
         return SK_PASS;
-
-    struct sockmap_key skm_key = {
-        .family = skb->family,
-        .remote_ip4 = skb->remote_ip4,
-        .local_ip4  = skb->local_ip4,
-        .remote_port  = bpf_ntohl(skb->remote_port),
-        .local_port = skb->local_port,
-    };
     
     int ret;
-    ret = bpf_sk_redirect_hash(skb, &sockmap_ops, &skm_key, 0); 
+    if (skb->remote_ip4 == skb->local_ip4) {
+        struct sockmap_key skm_key = {
+            .family = skb->family,
+            .remote_ip4 = skb->remote_ip4,
+            .local_ip4  = skb->local_ip4,
+            .remote_port  = skb->local_port,
+            .local_port = bpf_ntohl(skb->remote_port),
+        };
+        ret = bpf_sk_redirect_hash(skb, &sockmap_ops, &skm_key,
+                                   BPF_F_INGRESS);
+    } else {
+        struct sockmap_key skm_key = {
+            .family = skb->family,
+            .remote_ip4 = skb->remote_ip4,
+            .local_ip4 = skb->local_ip4,
+            .remote_port = bpf_ntohl(skb->remote_port),
+            .local_port = skb->local_port,
+        };
+        ret = bpf_sk_redirect_hash(skb, &sockmap_ops, &skm_key, 0);
+    } 
     
     if (ret != SK_PASS)
         bpf_printk("bpf_sk_redirect_hash() failed %d, error \n", -ret);
-    
-    bpf_printk("bpf_msg_redirect_hash() failed %d, error %d --> %d\n", -ret, bpf_ntohl(skb->remote_port), skb->local_port);
         
     return ret;   
 }
